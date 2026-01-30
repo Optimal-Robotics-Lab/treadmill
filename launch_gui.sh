866c924388acf8c0eecd94a7fa8c9033706283da
NaCl-Salt-12 <cla21052@byui.edu>
2026-01-30 14:53:29 -0500

    updated launch_gui script with sourceing and set ROS domain id

--- launch_gui.sh
+++ launch_gui.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+# Set ROS2 domain ID
+export ROS_DOMAIN_ID=11
+
+# Source ROS2
+source opt/ros/jazzy/setup.bash
+
 source install/setup.bash
 
 ros2 run treadmill_gui treadmill_gui_node
